4e1ac0df1ee93ec3010d3fcdd57ea5f3b5b58211
gaz647 <info@gaz647.eu>
2023-12-03 13:11:34 +0100

    Navbar nyní responzivní

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,69 @@
 import "./Navbar.css";
 import pages from "../assets/data";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 interface NavbarProps {
   visibleContainerId: number;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ visibleContainerId }) => {
+  const isMobile = window.innerWidth <= 580;
+
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
-    <nav className="navbar">
-      <ul>
-        {pages.map((onePage) => {
-          return (
-            <li key={onePage.id}>
-              <a
-                className={`${
-                  visibleContainerId.toString() === onePage.id &&
-                  "navbar-active"
-                }`}
-                href={`#${onePage.id}`}
-              >
-                {onePage.title}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+    <>
+      {!isMobile ? (
+        <nav className="navbar">
+          <ul>
+            {pages.map((onePage) => {
+              return (
+                <li key={onePage.id}>
+                  <a
+                    className={`${
+                      visibleContainerId.toString() === onePage.id &&
+                      "navbar-active"
+                    }`}
+                    href={`#${onePage.id}`}
+                  >
+                    {onePage.title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      ) : (
+        <nav className="navbar-mobile">
+          <div
+            className="navbar-mobile-icon-container"
+            onClick={() => setIsVisible(!isVisible)}
+          >
+            <RxHamburgerMenu className="navbar-mobile-icon" />
+          </div>
+          {isVisible && (
+            <ul>
+              {pages.map((onePage) => {
+                return (
+                  <li key={onePage.id}>
+                    <a
+                      className={`${
+                        visibleContainerId.toString() === onePage.id &&
+                        "navbar-active"
+                      }`}
+                      href={`#${onePage.id}`}
+                    >
+                      {onePage.title}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </nav>
+      )}
+    </>
   );
 };
 
