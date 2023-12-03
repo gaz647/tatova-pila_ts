8a86e15f23dc061535f7b253750b3f9b767104b8
gaz647 <info@gaz647.eu>
2023-12-03 12:03:31 +0100

    upraveny cesty k obrázkům 2

--- src/assets/data.ts
+++ src/assets/data.ts
@@ -3,25 +3,29 @@ const data = [
     id: "0",
     title: "O nás",
     text: "Lorem ipsum dolores clan",
-    image: "./src/assets/pictures/page1.jpg",
+    // image: "./src/assets/pictures/page1.jpg",
+    image: "/page1.jpg",
   },
   {
     id: "1",
     title: "Co děláme",
     text: "Lorem ipsum dolores clan",
-    image: "./src/assets/pictures/page2.jpg",
+    // image: "./src/assets/pictures/page2.jpg",
+    image: "/page2.jpg",
   },
   {
     id: "2",
     title: "Co neděláme",
     text: "Lorem ipsum dolores clan",
-    image: "./src/assets/pictures/page3.jpg",
+    // image: "./src/assets/pictures/page3.jpg",
+    image: "/page3.jpg",
   },
   {
     id: "3",
     title: "Kontakt",
     text: "Lorem ipsum dolores clan",
-    image: "./src/assets/pictures/page4.jpg",
+    // image: "./src/assets/pictures/page4.jpg",
+    image: "/page4.jpg",
   },
 ];
 
