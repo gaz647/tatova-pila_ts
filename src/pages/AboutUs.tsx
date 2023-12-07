5258e7eb690f260004b56c19fc84d622dddcf441
gaz647 <info@gaz647.eu>
2023-12-07 22:24:36 +0100

    Upraven text about us a změně font h1 a velikost text-container

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -7,40 +7,24 @@ const AboutUs: React.FC = () => {
         <h1>Jsme pilaři a jsme z Litomyšle</h1>
         <br />
         <ul>
-          <li>Zabýváme se výrobou stavebního řeziva.</li>
-          <br />
           <li>
-            Naše hlavní zaměření je spotřebitel - malovlastník lesa, který
-            potřebuje pořez u něho doma.
-          </li>
-          <br />
-          <li>Zabýváme se výrobou stavebního řeziva.</li>
-          <br />
-          <li>
-            Disponujeme pojízdnou pásovou pilou, se kterou přijedeme přímo k Vám
-            a pořežeme přímo u Vás ve dvoře.
-          </li>
-          <br />
-          <li>Stačí nám k tomu jen elektrická zásuvka na 380V.</li>
-          <br />
-          <li>Délka pořezu od 1,5m do 6m, průměr do 65cm.</li>
-          <br />
-          <li>Na místě provádíme pořez s co nejvyšší výtěžností.</li>
-          <br />
-          <li>Preferujeme pořez minimálne nad 5cbm v kulatině.</li>
-          <br />
-          <li>
-            Nebráníme se „speciálním“ přáním týkající se pořezu, variabilitu nám
-            naše pila umožňuje.
+            Svou prací cílíme především na vlastníka lesa, jenž potřebuje
+            pořezat svou kulatinu/výřezy na pilařské polotovary.{" "}
           </li>
           <br />
           <li>
-            Máme i vlastní les, tedy jsme schopni z vlastních zdrojů dodat
-            stavební řezivo, jakýkoliv sortiment.
+            Disponujeme pojízdnou pásovou pilou, se kterou přijedeme přímo k vám
+            a provedeme pořez dle vašeho přání.
           </li>
           <br />
-          <li>Práce se dřevem a s lidmi nás baví.</li>
+          <li>
+            Naší podmínkou je minimální množství kulatiny 5cbm v délce 1.5-6m,
+            do průměru 65cm.
+          </li>
+          <br />
+          <li>Nemáte les? nevadí! Dodáme vám z vlastního!</li>
         </ul>
+        <br />
         <h3>Zkuste to s námi a náš „zubatý cirkus“ nařeže i Vám {`:)`}</h3>
         <h3>Vaše Tátova Pila</h3>
       </div>
