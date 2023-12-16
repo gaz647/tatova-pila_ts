bee2fae5d5f8e9db2afc86166c2176973ebe20bc
gaz647 <info@gaz647.eu>
2023-12-16 18:44:47 +0100

    Zpět verze bez FB

--- src/components/FacebookPage.tsx
+++ src/components/FacebookPage.tsx
@@ -1,15 +1,7 @@
 import "./FacebookPage.css";
-import { FacebookProvider, Page } from "react-facebook";
 
 const FacebookPage = () => {
-  return (
-    <FacebookProvider appId="61554686402211">
-      <Page
-        href="https://www.facebook.com/profile.php?id=61554686402211"
-        tabs="timeline"
-      />
-    </FacebookProvider>
-  );
+  return <></>;
 };
 
 export default FacebookPage;
--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,24 +1,8 @@
 import "./ContactUs.css";
-import { useEffect } from "react";
 import ContactCard from "../components/ContactCard";
 import HonzaImage from "../assets/pictures/profile-image.png";
-import FacebookPage from "../components/FacebookPage";
 
 const ContactUs = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-
-    script.src =
-      "https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v18.0";
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <div className="page-container contact-us dis-flex-row-center relative">
       <div id="fb-root"></div>
@@ -34,59 +18,6 @@ const ContactUs = () => {
         </div>
 
         <h2 className="text-shadow-black">tatovapila@gmail.com</h2>
-      </div>
-      <div className="text-container">
-        <div
-          className="fb-page"
-          data-href="https://www.facebook.com/profile.php?id=61554686402211"
-          data-tabs="timeline"
-          data-width=""
-          data-height=""
-          data-small-header="false"
-          data-adapt-container-width="true"
-          data-hide-cover="false"
-          data-show-facepile="true"
-        >
-          <blockquote
-            cite="https://www.facebook.com/profile.php?id=61554686402211"
-            className="fb-xfbml-parse-ignore"
-          >
-            <a href="https://www.facebook.com/profile.php?id=61554686402211">
-              Tátova Pila
-            </a>
-          </blockquote>
-        </div>
-      </div>
-      <div
-        className="fb-page"
-        data-href="https://www.facebook.com/profile.php?id=61554686402211"
-        data-tabs="timeline"
-        data-width=""
-        data-height=""
-        data-small-header="false"
-        data-adapt-container-width="true"
-        data-hide-cover="false"
-        data-show-facepile="true"
-      >
-        <blockquote
-          cite="https://www.facebook.com/profile.php?id=61554686402211"
-          className="fb-xfbml-parse-ignore"
-        >
-          <a href="https://www.facebook.com/profile.php?id=61554686402211">
-            Tátova Pila
-          </a>
-        </blockquote>
-      </div>
-      <script
-        async
-        defer
-        crossOrigin="anonymous"
-        src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v18.0"
-        nonce="VQ43Bv10"
-      ></script>
-      <FacebookPage />
-      <div className="facebook-page-container">
-        <FacebookPage />
       </div>
     </div>
   );
