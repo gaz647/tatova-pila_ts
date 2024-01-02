ab450e0a9e175afe806955b98e9e945516ca4c45
gaz647 <info@gaz647.eu>
2024-01-02 18:19:08 +0100

    upraven text v WhereWeWork.tsx

--- src/pages/WhereWeWork.tsx
+++ src/pages/WhereWeWork.tsx
@@ -9,13 +9,6 @@ const WhereWeWork: React.FC = () => {
 
         <div className="text-container-text">
           <div className="text-container-text-sentence">
-            Pilku máme mobilní, tzn. že přijedeme k Vám.
-          </div>
-          <div className="text-container-text-sentence">
-            Podmínkou je pouze elektrická zásuvka na 380V a množství kulatiny
-            5cmb a více.
-          </div>
-          <div className="text-container-text-sentence">
             Žijeme v Litomyšli, takže náš rajón je Svitavsko.
           </div>
           <div className="text-container-text-sentence">
