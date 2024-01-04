f95b03f3abbd80171e0b8d8a48eaa586c7c25b18
gaz647 <info@gaz647.eu>
2024-01-04 15:03:26 +0100

    funkční lazy loading

---
+++ src/componentsExports.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const PageSection = React.lazy(() => import("./components/PageSection"));
+
+export const AboutUs = React.lazy(() => import("./pages/AboutUs"));
+export const WhatWeDo = React.lazy(() => import("./pages/WhatWeDo"));
+export const WhereWeWork = React.lazy(() => import("./pages/WhereWeWork"));
+export const Pricing = React.lazy(() => import("./pages/Pricing"));
+export const Gallery = React.lazy(() => import("./pages/Gallery"));
+export const ContactUs = React.lazy(() => import("./pages/ContactUs"));
+export const NotFound = React.lazy(() => import("./pages/NotFound"));
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from './App.tsx'
 import "./index.css";
 import {
   Route,
@@ -8,15 +7,17 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
+
 import Layout from "./Layout.tsx";
-import AboutUs from "./pages/AboutUs.tsx";
-import WhatWeDo from "./pages/WhatWeDo.tsx";
-import WhereWeWork from "./pages/WhereWeWork.tsx";
-import Pricing from "./pages/Pricing.tsx";
-import Gallery from "./pages/Gallery.tsx";
-import ContactUs from "./pages/ContactUs.tsx";
-import NotFound from "./pages/NotFound.tsx";
-import PageSection from "./components/PageSection.tsx";
+
+import { PageSection } from "./componentsExports.ts";
+import { AboutUs } from "./componentsExports.ts";
+import { WhatWeDo } from "./componentsExports.ts";
+import { WhereWeWork } from "./componentsExports.ts";
+import { Pricing } from "./componentsExports.ts";
+import { Gallery } from "./componentsExports.ts";
+import { ContactUs } from "./componentsExports.ts";
+import { NotFound } from "./componentsExports.ts";
 
 const screenWidth = window.innerWidth;
 
@@ -26,49 +27,61 @@ const router = createBrowserRouter(
       <Route
         path="o-nas"
         element={
-          <PageSection image={"/about-us"} screenWidth={screenWidth}>
-            <AboutUs />
-          </PageSection>
+          <React.Suspense fallback={<>kokot</>}>
+            <PageSection image={"/about-us"} screenWidth={screenWidth}>
+              <AboutUs />
+            </PageSection>
+          </React.Suspense>
         }
       />
       <Route
         path="co-delame"
         element={
-          <PageSection image={"/what-we-do"} screenWidth={screenWidth}>
-            <WhatWeDo />
-          </PageSection>
+          <React.Suspense fallback={<>kokot</>}>
+            <PageSection image={"/what-we-do"} screenWidth={screenWidth}>
+              <WhatWeDo />
+            </PageSection>
+          </React.Suspense>
         }
       />
       <Route
         path="kde-pracujeme"
         element={
-          <PageSection image={"/where-we-work"} screenWidth={screenWidth}>
-            <WhereWeWork />
-          </PageSection>
+          <React.Suspense fallback={<>kokot</>}>
+            <PageSection image={"/where-we-work"} screenWidth={screenWidth}>
+              <WhereWeWork />
+            </PageSection>
+          </React.Suspense>
         }
       />
       <Route
         path="za-kolik"
         element={
-          <PageSection image={"/pricing"} screenWidth={screenWidth}>
-            <Pricing />
-          </PageSection>
+          <React.Suspense fallback={<>kokot</>}>
+            <PageSection image={"/pricing"} screenWidth={screenWidth}>
+              <Pricing />
+            </PageSection>
+          </React.Suspense>
         }
       />
       <Route
         path="galerie"
         element={
-          <PageSection image={"/gallery"} screenWidth={screenWidth}>
-            <Gallery />
-          </PageSection>
+          <React.Suspense fallback={<>kokot</>}>
+            <PageSection image={"/gallery"} screenWidth={screenWidth}>
+              <Gallery />
+            </PageSection>
+          </React.Suspense>
         }
       />
       <Route
         path="kontakt"
         element={
-          <PageSection image={"/contact-us"} screenWidth={screenWidth}>
-            <ContactUs />
-          </PageSection>
+          <React.Suspense fallback={<>kokot</>}>
+            <PageSection image={"/contact-us"} screenWidth={screenWidth}>
+              <ContactUs />
+            </PageSection>
+          </React.Suspense>
         }
       />
       <Route path="*" element={<NotFound />} />
