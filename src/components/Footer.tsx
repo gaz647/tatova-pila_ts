b86f4d328fa6b4af4d4a86cd2e7fbff82bb7496f
gaz647 <info@gaz647.eu>
2024-09-26 20:52:36 +0200

    přidáno logo bezstarosti

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 import "./Footer.css";
 import LogoMadeInLitomysl from "../assets/pictures/made-in-litomysl-white.png";
+import { TbWorldWww } from "react-icons/tb";
+import LogoBezStarosti from "../assets/pictures/bez-starosti.png";
 
 const Footer: React.FC = () => {
   return (
@@ -11,8 +13,20 @@ const Footer: React.FC = () => {
           className="made-in-litomysl"
         />
       </a>
-      <a href="https://gaz647.net/" className="gaz-647">
-        gaz647.net
+      <a href="https://gaz647.net/">
+        <div className="gaz647-logo">
+          <TbWorldWww className="gaz47-logo-planet" />
+          <p className="gaz647-logo-text">
+            by: <span className="gaz-logo-text-bold">gaz647</span>
+          </p>
+        </div>
+      </a>
+      <a href="https://www.bezstarosti-video.cz/">
+        <img
+          src={LogoBezStarosti}
+          alt="logo-bez-starosti"
+          className="bez-starosti"
+        />
       </a>
     </footer>
   );
